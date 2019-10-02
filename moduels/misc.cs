4c54e8647ae099c123684f552e265499587efc74
Nextonix <34287776+Nextonix@users.noreply.github.com>
2019-10-02 15:43:18 +0200

    Add files via upload

---
+++ moduels/misc.cs
@@ -0,0 +1,41 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace cussbot.moduels
+{
+    public class Misc : ModuleBase<SocketCommandContext>
+    {
+        [Command("echo")]
+        public async Task Echo([Remainder]string message)
+        {
+            var embed = new EmbedBuilder();
+            embed.WithTitle("Message by " + Context.User.Username);
+            embed.WithDescription(message);
+            embed.WithColor(new Color(67, 67, 67));
+            await Context.Channel.SendMessageAsync("", false, embed.Build());
+
+        }
+
+        [Command("pick")]
+        public async Task PickOne([Remainder]string message)
+        {
+            string[] options = message.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+
+            Random r = new Random();
+            string selection = options[r.Next(0, options.Length)];
+
+            var embed = new EmbedBuilder();
+            embed.WithTitle("Choice for " + Context.User.Username);
+            embed.WithDescription(selection);
+            embed.WithColor(new Color(67, 67, 67));
+            embed.WithThumbnailUrl("https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.pngall.com%2Fwp-content%2Fuploads%2F2016%2F04%2FDice-Free-Download-PNG.png&f=1&nofb=1");
+            await Context.Channel.SendMessageAsync("", false, embed.Build());
+
+        }
+    }
+}
