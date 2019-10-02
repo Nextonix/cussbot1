2dde1027e5e050db830812fbbb0b28429bd756d4
Nextonix <34287776+Nextonix@users.noreply.github.com>
2019-10-02 02:58:15 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+
+namespace cussbot
+{
+    class Program
+    {
+        DiscordSocketClient _client;
+        commandHandler _handler;
+
+        static void Main(string[] args)
+        => new Program().StartAsync().GetAwaiter().GetResult();
+
+        public async Task StartAsync()
+        {
+            if (Config.bot.token == "" || Config.bot.token == null) return;
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Verbose
+            });
+            _client.Log += Log;
+            _handler = new commandHandler();
+            await _client.LoginAsync(TokenType.Bot, Config.bot.token);
+            await _client.StartAsync();
+            _handler = new commandHandler();
+            await _handler.InitilizeAsync(_client);
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.Message);
+            return Task.CompletedTask;
+        }
+    }
+}
